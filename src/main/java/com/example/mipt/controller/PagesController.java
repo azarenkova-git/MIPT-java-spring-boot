4d634c314013c9dbade029466c8c8821bd39c1d8
svetlana <ivanova.sd@phystech.edu>
2023-05-30 00:47:48 +0300

    Add spring security, controllers tests

--- src/main/java/com/example/mipt/controller/PagesController.java
+++ src/main/java/com/example/mipt/controller/PagesController.java
@@ -1,30 +1,54 @@
 package com.example.mipt.controller;
 
 import com.example.mipt.dto.UserDto;
+import com.example.mipt.models.UserModel;
 import com.example.mipt.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class PagesController {
     final private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public PagesController(UserRepository userRepository) {
+    public PagesController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/createUser")
     public String createUserForm(Model model) {
         model.addAttribute("user", new UserDto());
-        return "createUser";
+        return "/createUser";
+    }
+
+    @PostMapping("/api/user/create")
+    public RedirectView createUser(@ModelAttribute UserDto userDto) {
+        UserModel user = new UserModel();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        userRepository.save(user);
+        return new RedirectView("/users");
     }
 
     @GetMapping("/users")
     public String users(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        List<UserModel> users = userRepository.findAll();
+        model.addAttribute("users", users);
         return "users";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
